7f479de5879c872b4a4ecb160cf29c7746e20c0b
Amanda Nisenbaum <amanda.nisenbaum@knights.ucf.edu>
2021-10-21 16:42:16 -0400

    added login page

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './screens/login';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       <Text>Hi Friends - Amanda</Text>
       <StatusBar style="auto" />
+      <Login/>
     </View>
   );
 }
