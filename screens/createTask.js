ce7a2913b1318989bfde268a56ac7cd9c0f7adf7
Andrew-Turpin <71479812+Andrew-Turpin@users.noreply.github.com>
2021-11-23 14:13:04 -0500

    Edit on the createtask
    running into error post submission, screenshot in discord

--- screens/createTask.js
+++ screens/createTask.js
@@ -25,10 +25,41 @@ export default function createTask() {
     //api call to submit the data in the form
 
     //console log to verify inputs for debugging
+
+    postData("https://helpinghand-cop4331.herokuapp.com/task/create", {
+      name: data.name,
+      description: data.details,
+      max_slots: data.volunteers,
+      latitude: 28.6,
+      longitude: 81.2,
+      coordID: '618abb37a863429df1555084' ,
+    }).then((data) => {
+      console.log(data); // JSON data parsed by `data.json()` call
+    });
+
     console.log(data.name, data.details, data.volunteers, data.location);
     navigateToTaskList();
   }
 
+  // Example POST method implementation:
+  async function postData(url = '', data = {}) {
+    console.log(url, data);
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
   //function to navigate back to task list on cancel
   function navigateToTaskList() {
     navigation.navigate("CoordinatorTasks");
