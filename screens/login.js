fbe14bdab781ea15f2c74b28dfaaf53206a688d5
harrismichael2095 <45339433+harrismichael2095@users.noreply.github.com>
2021-11-27 03:01:08 -0500

    login api connected
    trying to figure out how to display a coord/vol list with ID

--- screens/login.js
+++ screens/login.js
@@ -98,22 +98,42 @@ const SignInScreen = ({ navigation }) => {
 
   const loginHandle = (userName, password) => {
     console.log(data.role)
-    // const foundUser = Users.filter( item => {
-    //     return userName == item.username && password == item.password;
-    // } );
-    // if ( data.username.length == 0 || data.password.length == 0 ) {
-    //     Alert.alert('Wrong Input!', 'Username or password field cannot be empty.', [
-    //         {text: 'Okay'}
-    //     ]);
-    //     return;
-    // }
-    // if ( foundUser.length == 0 ) {
-    //     Alert.alert('Invalid User!', 'Username or password is incorrect.', [
-    //         {text: 'Okay'}
-    //     ]);
-    //     return;
-    // }
-    // signIn(foundUser);
+     //console.log(userName)
+    //console.log(password)
+    console.log(data.role)
+      async function login(){
+        var obj = {email:userName,password:password};
+        var js=JSON.stringify(obj);
+        //console.log(js);
+        try{
+          const response = await fetch("https://helpinghand-cop4331.herokuapp.com/vol/login",{
+            method: "POST",
+            headers: {"Content-Type" : "application/json"},
+            body : js,
+          });
+          var res=JSON.parse(await response.text() );
+          if (res.error != null){
+            console.log(res.error);
+          } else{   
+            if(res.id!=-1 && data.role == "coordinator")
+            navigation.navigate("CoordinatorTasks");
+
+            else if(res.id!=-1&& data.role == "volunteer")
+            navigation.navigate("VolunteerTasks");
+            else if(res.id== -1)
+            alert("incorrect username or password")
+          }
+            console.log(res)
+            console.log(res.id)
+        } catch (e){
+          alert(e.toString());
+          return ;
+        }
+      }
+        //test
+      login();
+   ;
+    //handleMessage(null);
   };
 
   return (
